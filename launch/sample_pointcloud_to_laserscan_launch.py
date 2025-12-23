ed82ba4baf72ff57505e55a915de2950a609317a
omd98 <mahmoud@graduate.utm.my>
2025-12-23 10:24:41 +0800

    edit the frame

--- launch/sample_pointcloud_to_laserscan_launch.py
+++ launch/sample_pointcloud_to_laserscan_launch.py
@@ -10,22 +10,22 @@ def generate_launch_description():
             name='scanner', default_value='scanner',
             description='Namespace for sample topics'
         ),
-        Node(
-            package='pointcloud_to_laserscan', executable='dummy_pointcloud_publisher',
-            remappings=[('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud'])],
-            parameters=[{'cloud_frame_id': 'cloud', 'cloud_extent': 2.0, 'cloud_size': 500}],
-            name='cloud_publisher'
-        ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_transform_publisher',
-            arguments=[
-                '--x', '0', '--y', '0', '--z', '0',
-                '--qx', '0', '--qy', '0', '--qz', '0', '--qw', '1',
-                '--frame-id', 'map', '--child-frame-id', 'cloud'
-            ]
-        ),
+        # Node(
+        #     package='pointcloud_to_laserscan', executable='dummy_pointcloud_publisher',
+        #     remappings=[('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud'])],
+        #     parameters=[{'cloud_frame_id': 'cloud', 'cloud_extent': 2.0, 'cloud_size': 500}],
+        #     name='cloud_publisher'
+        # ),
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='static_transform_publisher',
+        #     arguments=[
+        #         '--x', '0', '--y', '0', '--z', '0',
+        #         '--qx', '0', '--qy', '0', '--qz', '0', '--qw', '1',
+        #         '--frame-id', 'map', '--child-frame-id', 'cloud'
+        #     ]
+        # ),
         Node(
             package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
             remappings=[('cloud_in', [LaunchConfiguration(variable_name='scanner'), '/cloud']),
@@ -40,7 +40,7 @@ def generate_launch_description():
                 'angle_increment': 0.0087,  # M_PI/360.0
                 'scan_time': 0.3333,
                 'range_min': 0.45,
-                'range_max': 4.0,
+                'range_max': 60.0,
                 'use_inf': True,
                 'inf_epsilon': 1.0
             }],
